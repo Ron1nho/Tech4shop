d4f780d1a15b65579e3595586ae8a9c7d4bd7597
Ron1nho <ronifebrone9@gmail.com>
2023-03-29 23:40:46 -0300

    Tech4shop

--- Tech4shop/roupas/src/test/java/br/com/tech4shop/loja/LojaApplicationTests.java
+++ Tech4shop/roupas/src/test/java/br/com/tech4shop/loja/LojaApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.tech4pizza.pizzaria;
+package br.com.tech4shop.loja;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PizzariaApplicationTests {
+class LojaApplicationTests {
 
 	@Test
 	void contextLoads() {
